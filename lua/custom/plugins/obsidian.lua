5c49c7aed2b1aa13ebcb0f269b10d404d087510e
Jamiel van Engen <jamiel.van.engen@gmail.com>
2025-12-21 21:41:10 +0100

    Changed the config for the obsidian.lua to make sure the new_note_from_template function will behave as I expect (it will save it to my Notes/ directory, with the title as the filename and without the extra frontmatter stuff)

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -5,12 +5,23 @@ return {
   ---@module 'obsidian'
   ---@type obsidian.config
   opts = {
+    frontmatter = {
+      enabled = false,
+    },
+    note_id_func = function(title)
+      return title
+    end,
     legacy_commands = false, -- this will be removed in the next major release
+    new_notes_location = 'notes_subdir',
+    notes_subdir = 'Notes',
     workspaces = {
       {
         name = 'Second Brain',
         path = '~/Documents/Second-Brain',
       },
+    },
+    templates = {
+      folder = 'Other/Templates',
     },
   },
 }
